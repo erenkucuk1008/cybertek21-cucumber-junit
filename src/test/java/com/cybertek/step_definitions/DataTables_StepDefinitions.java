143c1a4777fc41277044bf6eab89a580bb2da139
erenkucuk <erenkkucuk89@gmail.com>
2022-08-22 21:44:04 -1000

    added new class and lines

--- src/test/java/com/cybertek/step_definitions/DataTables_StepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/DataTables_StepDefinitions.java
@@ -1,10 +1,19 @@
 package com.cybertek.step_definitions;
 
+import com.cybertek.pages.SmartBearLoginPage;
+import com.cybertek.utilities.BrowserUtils;
+import com.cybertek.utilities.ConfigurationReader;
+import com.cybertek.utilities.Driver;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 import java.util.List;
+import java.util.Map;
 
 public class DataTables_StepDefinitions {
+
+    SmartBearLoginPage smartBearLoginPage = new SmartBearLoginPage();
 
     @Then("user should see below words displayed")
     public void user_should_see_below_words_displayed(List<String> fruitLists) {
@@ -15,5 +24,34 @@ public class DataTables_StepDefinitions {
         for (String eachFruit : fruitLists) {
             System.out.println("eachFruit = " + eachFruit);
         }
+    }
+
+
+
+    @Given("User is on the login page of SmartBear")
+    public void user_is_on_the_login_page_of_smart_bear() {
+
+        String url = ConfigurationReader.getProperty("smartBearUrl");
+        Driver.getDriver().get(url);
+    }
+
+
+    @When("User enters below info")
+    public void user_enters_below_info(Map<String, String> loginInfo) {
+
+        //Map olusturuyoruz, cunku key and value olarak giriliyor\
+        //                     "username" ve "password" dataTable.feature dan geliyor
+        //                          bu iki key in value sunu al diyoruz
+        String username = loginInfo.get("username");// will return Tester
+        String password = loginInfo.get("password");// will return test
+
+        smartBearLoginPage.loginToSmartBear(username, password);
+    }
+
+
+    @Then("User should see title changed to Web Orders")
+    public void user_should_see_title_changed_to_web_orders() {
+
+        BrowserUtils.titleVerification("Web Orders");
     }
 }
--- src/test/resources/features/dataTables.feature
+++ src/test/resources/features/dataTables.feature
@@ -1,6 +1,14 @@
 Feature: Simple examples of how dataTables work
 
   @dataTable
+  Scenario: User should be able to login with correct credentials
+    Given User is on the login page of SmartBear
+    When User enters below info
+      | username | Tester |
+      | password | test   |
+    Then User should see title changed to Web Orders
+
+
   Scenario: Something happens
     #Given user does something
     Then user should see below words displayed
