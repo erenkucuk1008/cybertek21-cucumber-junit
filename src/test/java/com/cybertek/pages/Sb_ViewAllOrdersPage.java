fe16f82382d925c8842d4ae31f1621be02ed4b53
erenkucuk <erenkkucuk89@gmail.com>
2022-08-25 01:01:15 -1000

    added new class and lines

--- src/test/java/com/cybertek/pages/Sb_ViewAllOrdersPage.java
+++ src/test/java/com/cybertek/pages/Sb_ViewAllOrdersPage.java
@@ -3,8 +3,13 @@ package com.cybertek.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class Sb_ViewAllOrdersPage extends SbBasePage{
 
     @FindBy(xpath = "//tr[2]/td[2]")
     public WebElement nameInList;
+
+    @FindBy(xpath = "//td[2]")
+    public List<WebElement> listOfNames;
 }
